d24e1cf5b53f081375c5b0f6b933ae7020f220c5
Victor Isaac <tamsvictorisaac@gmail.com>
2021-10-15 05:48:56 +0100

    initial

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,13 +4,13 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["equesttech.ng"])
+ALLOWED_HOSTS = ['equesttech-lms-qncmq.ondigitalocean.app', "equesttech.ng", "www.equesttech.ng"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES["default"] = os.getenv("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
