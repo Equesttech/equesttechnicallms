13b4af7e47a1cf55b5f2980bc6744970c3cb2bc3
Victor Isaac <tamsvictorisaac@gmail.com>
2021-10-27 23:31:32 +0100

    initial

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 from django.urls import path
 from django.urls import include
@@ -9,4 +11,4 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='bloghome'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
